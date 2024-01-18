26e97c0e4c45832faa9a597f671248163f91801a
Kartikey Srivastava <kartikeyai246@gmail.com>
2024-01-18 23:41:40 +0530

    drawer council members

--- lib/Screens/Drawer/drawer.dart
+++ lib/Screens/Drawer/drawer.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-
-import 'package:omnia/Screens/Home/homescreen.dart';
 import 'package:omnia/Resources/Theme/theme.dart';
 
 class CustomDrawer extends StatefulWidget {
@@ -14,6 +12,7 @@ class _CustomDrawerState extends State<CustomDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      backgroundColor: navColor,
       child: ListView(
         padding: EdgeInsets.zero,
         children: <Widget>[
@@ -31,27 +30,59 @@ class _CustomDrawerState extends State<CustomDrawer> {
               ),
             ),
           ),
-          ListTile(
-            leading: const Icon(Icons.home),
-            title: const Text('Home'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const HomeScreen()),
-              );
-            },
+          // Custom card 1
+          _buildDrawerCard(
+            heading: 'Chairperson',
+            subheading: 'Position 1',
+            imagePath: 'assets/profile1.png',
           ),
-          // ListTile(
-          //   leading: const Icon(Icons.settings),
-          //   title: const Text('Community'),
-          //   onTap: () {
-          //     Navigator.push(
-          //       context,
-          //       MaterialPageRoute(builder: (context) => const Community()),
-          //     );
-          //   },
-          // ),
+          // Custom card 2
+          _buildDrawerCard(
+            heading: 'Vice Chairperson',
+            subheading: 'Position 2',
+            imagePath: 'assets/profile2.png',
+          ),
+          // Custom card 3
+          _buildDrawerCard(
+            heading: 'Webmaster',
+            subheading: 'Position 3',
+            imagePath: 'assets/profile3.png',
+          ),
+          // Custom card 4
+          _buildDrawerCard(
+            heading: 'Treasurer',
+            subheading: 'Position 4',
+            imagePath: 'assets/profile4.png',
+          ),
+          // Custom card 5
+          _buildDrawerCard(
+            heading: 'Secretary',
+            subheading: 'Position 5',
+            imagePath: 'assets/profile5.png',
+          ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildDrawerCard({
+    required String heading,
+    required String subheading,
+    required String imagePath,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        child: ListTile(
+          title: Text(heading),
+          subtitle: Text(subheading),
+          leading: CircleAvatar(
+            backgroundImage: AssetImage(imagePath),
+          ),
+          onTap: () {
+            // Handle card
+          },
+        ),
       ),
     );
   }
