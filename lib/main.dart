ee04173d1dcddb04511c4b0061cf400bce7c40c8
Kartikey Srivastava <kartikeyai246@gmail.com>
2024-01-28 00:24:55 +0530

    firebase cli done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-// import 'package:omnia/Signup/Splash/splash0.dart';
+import 'package:omnia/firebase_options.dart';
 import 'Screens/Signup/signuppagewolf.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
