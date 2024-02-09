cd0d473209762bf177a13e5c1299a4d65f1a5222
Kartikey Srivastava <kartikeyai246@gmail.com>
2024-02-10 00:06:25 +0530

    firebase signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:omnia/firebase_options.dart';
+
 import 'Screens/Signup/signuppagewolf.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
